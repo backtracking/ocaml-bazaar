748187f603064bc1441c33e7aff9e547b3b5a0ce
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2022-09-01 17:37:35 +0200

    palindromic hello world

---
+++ phw.ml
@@ -0,0 +1,6 @@
+(* Palindromic hello world.
+   This one is due to Guillaume Melquiond.
+   See also lm.ml for a palindromic OCaml source code
+   that prints something (but not "hello world").
+*)
+"!dlrow ,ollehr\" enildne_tnirp ;"; print_endline "\rhello, world!"
