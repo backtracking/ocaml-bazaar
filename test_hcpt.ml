09bb2539230876609c88d3732b49b76d15374808
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2024-02-16 20:43:12 +0100

    hcpt: more tests

--- test_hcpt.ml
+++ test_hcpt.ml
@@ -27,4 +27,63 @@ let test ?(limit=100) n =
 let () =
   for n = 0 to 100 do test n done
 
+(* using Map as a reference implementation *)
+module R = Map.Make(Int)
+
+let rand ?(limit=10) () =
+  match Random.int (2 * limit) with
+  | 0 -> min_int
+  | 1 -> max_int
+  | n -> n - limit
+
+let rec build r t n =
+  if n = 0 then r, t else
+  let k = rand () in
+  let v = string_of_int k in
+  build (R.add k v r) (M.add k v t) (n - 1)
+
+let random n =
+  build R.empty M.empty n
+
+let same_set r t =
+  assert (R.cardinal r = M.cardinal t);
+  R.iter (fun k v -> assert (M.mem k t); assert (M.find k t = v)) r;
+  M.iter (fun k v -> assert (R.mem k r); assert (R.find k r = v)) t
+
+let () =
+  for n = 0 to 20 do
+    let r, t = random n in
+    same_set r t;
+    assert (M.is_empty t = R.is_empty r);
+    assert (M.min_binding_opt t = R.min_binding_opt r);
+    assert (M.max_binding_opt t = R.max_binding_opt r);
+    assert (M.bindings t = R.bindings r);
+    for _ = 1 to 10 do
+      let k = rand () in
+      assert (M.mem k t = R.mem k r);
+      let rl, rv, rr = R.split k r in
+      let tl, tv, tr = M.split k t in
+      same_set rl tl;
+      assert (rv = tv);
+      same_set rr tr;
+    done;
+    for m = 1 to 10 do
+      let r', t' = random m in
+      let f _ s1 s2 = Some (s1 ^ s2) in
+      same_set (R.union f r r') (M.union f t t');
+      let f _ _ _ = Some "foo" in
+      same_set (R.merge f r r') (M.merge f t t');
+    done;
+    let r, t =
+      R.fold (fun k v (r, t) ->
+        let r = R.remove k r in
+        let t = M.remove k t in
+        same_set r t;
+        (r, t)
+      )
+      r (r, t) in
+    assert (R.is_empty r);
+    assert (M.is_empty t)
+  done
+
 let () = Format.printf "test hcpt OK@."
