1d043fab0e8406bc5aad2a01e246f80f2c13f80e
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@cnrs.fr>
2026-09-15 09:36:59 +0200

    fix vga_chaos

--- vga_chaos.ml
+++ vga_chaos.ml
@@ -6,27 +6,27 @@ open Vga
 
 let width = 320
 let height = 200
-let page = 0x10000 (* or even let page = width * height *)
+let scale = 4
 
+let page = 0x10000
 let mem = Array.make page 0
-let valid a = 0 <= a && a < page
-let get a   = if valid a then Array.unsafe_get mem a else 0
-let set a v = if valid a then Array.unsafe_set mem a v
+let get a   = Array.unsafe_get mem (a land (page-1))
+let set a v = Array.unsafe_set mem (a land (page-1)) v
 
 let () =
-  open_graph (Printf.sprintf " %dx%d" width height);
+  open_graph (Printf.sprintf " %dx%d" (scale * width) (scale * height));
   auto_synchronize false;
   let acc = ref 0 in
   while true do
     for i = 0 to page - 1 do
-      acc := !acc + get (i + 1) + get (i - 320) + get (i + 320);
+      acc := (!acc + get (i + 1) + get (i - 320) + get (i + 320)) land 0xFF;
       acc := (!acc lsr 2 + 1) land 0xFF;
       set i !acc;
       if i < width * height then (
         let y = height - 1 - i / width in
         let x = i mod width in
         set_color palette.(!acc);
-        plot x y
+        fill_rect (x * scale) (y * scale) scale scale
       )
     done;
     synchronize ()
